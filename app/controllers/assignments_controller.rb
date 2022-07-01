3cb2d0e8065910e31e837cd13209f5bd87aeac56
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-07-01 09:32:50 -0500

    Add whitespace

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -5,6 +5,7 @@ class AssignmentsController < ApplicationController
     def index
         render json: Assignment.all, status: :ok
     end
+    
     def show
         render json: Assignment.find(params[:id])
     end
