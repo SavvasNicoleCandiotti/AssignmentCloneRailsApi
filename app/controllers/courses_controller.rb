2c5f4e10a68678c2560c2955f40d1482df81d520
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-07-01 09:34:41 -0500

    Update action fully functioning

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,19 +1,32 @@
 class CoursesController < ApplicationController
     def index
-        render json: Course.all, status: :ok
+        render json: courses, status: :ok
     end
 
     def show
-        render json: Course.find(params[:id])
+        render json: course
     end
 
     def create 
         render json: Course.create!(course_params)
     end
 
+    def update
+        course.update!(course_params)
+        render json: course
+    end
+
     private
         
         def course_params
             params.require(:course).permit(:name, :gradeLevel)
+        end
+
+        def course
+            @course = Course.find(params[:id])
+        end
+
+        def courses
+            @courses = Course.all
         end
 end
