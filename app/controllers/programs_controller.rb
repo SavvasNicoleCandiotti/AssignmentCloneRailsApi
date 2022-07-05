ce5e76d22b4d2e2fe0f028d66cb5bf874a267a01
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-07-05 11:12:11 -0500

    Create actions for programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,29 @@
 class ProgramsController < ApplicationController
+
+     def index
+        render json: programs, status: :ok
+    end
+    
+    def show
+        render json: program
+    end
+
+    def create 
+        @program = Program.create!(programs_params)
+        render json: @program, status: :created
+    end
+
+private
+
+    def programs_params 
+        params.require(:program).permit(:name, :gradeLevel, :is_common_core)
+    end
+
+    def program
+        @program = Program.find(params[:id])
+    end
+
+    def programs
+        @programs = Program.all
+    end
 end
